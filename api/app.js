aa77bf8d03d57c91dfba7e3920873a1e4fdb7ac0
langloise <etiennelanglois007@gmail.com>
2023-12-17 11:38:21 -0500

    Début du websocket

--- api/app.js
+++ api/app.js
@@ -1,11 +1,35 @@
 const express = require('express');
-
+const websocketServer = require('websocket').server;
 const app = express();
 
 const port = 3000;
 
-// code …
 
 app.listen(port, () => {
     console.log(`Le serveur écoute sur le port ${port}`)
   })
+
+const wsServer = new websocketServer({
+    httpServer: app
+})
+wsServer.on('request', request => {
+    const connection = request.accept(null, request.origin);
+    console.log('Nouvelle connexion');
+    connection.on('open', () => {
+        console.log('Connexion ouverte');
+    })
+    connection.on('close', () => {
+        console.log('Connexion fermée');
+    })
+    connection.on('message', message => {
+        console.log(`Reçu : ${message.utf8Data}`);
+        connection.sendUTF('Salut client');
+    })
+
+    const clientId = guid();
+})
+
+const guid=()=> {
+  const s4=()=> Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);     
+  return `${s4() + s4()}-${s4()}-${s4()}-${s4()}-${s4() + s4() + s4()}`;
+}
